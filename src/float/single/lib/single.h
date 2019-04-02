c22ad9dd1947e7130ffd279a574e1cf893c961d5
mgurski <48213269+mgurski@users.noreply.github.com>
2019-04-02 23:39:19 +0200

    Add files via upload

---
+++ src/float/single/lib/single.h
@@ -0,0 +1,73 @@
+#include <cinttypes>
+#include <string>
+
+//extern "C" void addition(float *, float, float);
+
+namespace floating
+{
+
+class Single
+{
+public:
+  /**
+   * Contructors
+   */
+  Single(){};
+  Single(float f);
+  Single(long double longDouble);
+  Single(unsigned long long uLongLong);
+  Single(const Single &s);
+  Single(uint8_t a, uint8_t b, uint8_t c, uint8_t d)
+      : a(a), b(b), c(c), d(d){};
+
+  /**
+  * Conversions
+  */
+  operator int();
+  operator float();
+  operator double();
+
+  int toInt();
+  float toFloat();
+  double toDouble();
+
+  /**
+   * Math
+   */
+  Single operator-();
+  Single abs();
+  Single changeSign();
+  Single uglyAddition(Single, Single);
+  /**
+   * Comparison operators
+   */
+  bool operator==(const Single &s);
+
+  /**
+   * Utils, debug, etc.
+   */
+  std::string printBinary();
+  std::string printExponent(bool binary = false);
+  std::string printFraction(bool binary = false);
+
+private:
+  /**
+   *         sign           fraction
+   * Format: d | dddddddc | cccccccbbbbbbbbaaaaaaaa
+   *             exponent
+   * Float in memory looks like:
+   * [aaaaaaaabbbbbbbbccccccccdddddddd]
+   */
+  uint8_t d = 0, c = 0, b = 0, a = 0;
+  // dać tu unię
+
+  void initFromFloat(float);
+};
+
+namespace literal
+{
+Single operator""_s(long double longDouble);
+Single operator""_s(unsigned long long uLongLong);
+} // namespace literal
+
+} // namespace floating
