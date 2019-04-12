46a6531b227a0c92908ae80a10738e1b9f016943
mgurski <mateuszgurski444@gmail.com>
2019-04-13 00:12:40 +0200

    Pierwiastek

--- src/float/single/math/squareroot.s
+++ src/float/single/math/squareroot.s
@@ -5,24 +5,50 @@ single_sqrt:
 
 	pushl	%ebp
 	pushl	%edi
+    pushl   %esi
 	movl	%esp, %ebp
 
-	subl 	$0xe, %esp
+	subl 	$0xd, %esp
 
-# | float a			|  0x10(%ebp)
-# | float *wynik    |  0xc(%ebp) 
-# | ret addr		|  0x8(%ebp)
-# | kopia EBP		|  0x4(%ebp)
-# | kopia EDI		|  0x0(%ebp)
-# | Liczba			| -0x4(%ebp)
-# | Qi  			| -0x8(%ebp)
-# | Ri              | -0xc(%ebp)
-# | licznik_q       | -0xd(%ebp)
-# | licznik_r       | -0xe(%ebp)
+    movl 0x14(%ebp), %edi
+    movl %edi, -0x8(%ebp)
+    movl %ebp, %edi
+    subl $8, %edi
+
+    movl %ebp, %esi
+    subl $4, %esi
+
+    movl %ebp, %ebx
+    subl $12, %ebx
+
+    xor %ecx, %ecx
+
+# | float a			|  0x14(%ebp)
+# | float *wynik    |  0x10(%ebp) 
+# | ret addr		|  0xc(%ebp)
+# | kopia EBP		|  0x8(%ebp)
+# | kopia EDI		|  0x4(%ebp)
+# | kopia ESI       |  0x0(%ebp)
+# | Ri  			| -0x4(%ebp)
+# | Liczba  		| -0x8(%ebp)
+# | Qi              | -0xc(%ebp)
+# | EXP             | -0xd(%ebp)
+
+    movb $0, (%esi, %ecx, 1)
+    movb $0, (%ebx, %ecx, 1)
+    addb $1, %cl
+    movb $0, (%esi, %ecx, 1)
+    movb $0, (%ebx, %ecx, 1)
+    addb $1, %cl
+    movb $0, (%esi, %ecx, 1)
+    movb $0, (%ebx, %ecx, 1)
+    addb $1, %cl
+    movb $0, (%esi, %ecx, 1)
+    movb $0, (%ebx, %ecx, 1)
 
 # Sprawdzam czy liczba jest dodatnia
-    movb 0x13(%ebp), %al # d
-    movb 0x12(%ebp), %cl # c
+    movb 0x17(%ebp), %al # d
+    movb 0x16(%ebp), %cl # c
     shlb $1, %cl
     rclb $1, %al
     jc UNDOABLE
@@ -39,424 +65,163 @@ UNDOABLE:
 
     movl	%ebp, %esp
 	popl	%edi
+    popl    %esi
 	popl	%ebp
 	ret
 
 ODD:
 
-subb $1, %al
-shrb $1, %al
-addb $127, %al
+    subb $1, %al
+    shrb $1, %al
+    addb $127, %al
 
-movb %al, -0x1(%ebp)
+    movb %al, -0xd(%ebp)
 
 # wykładnik gotowy
 # Przesunięcie mantysy w lewo żeby pozbyć się najstarszego bitu, który jest częścią wykładnika
-movb 0x12(%ebp), %cl # c
-movb 0x11(%ebp), %dl # b
-movb 0x10(%ebp), %ch # a
 
-shlb $1, %ch
-rclb $1, %dl
-rclb $1, %cl
+    push $9
+    push %edi
+    call simple_shiftL
 
-movb %cl, 0x12(%ebp)
-movb %dl, 0x11(%ebp)
-movb %ch, 0x10(%ebp)
+    push $1
+    push %edi
+    call simple_shiftR
 
-jmp FIRST_CASE
+    movb $3, %cl
+    addb $128, (%edi, %ecx, 1)
+
+    push $2
+    push %edi
+    call simple_shiftL_64
+
+    movb $0, %cl
+    addb $1, (%ebx, %ecx, 1)
+
+    push %ebx
+    push %esi
+    call simple_sub
+
+
+    jmp ALGORITHM_LOOP
 
 EVEN:
 
-shrb $1, %al
-addb $127, %al
+    shrb $1, %al
+    addb $127, %al
 
-movb %al, -0x1(%ebp)
+    movb %al, -0xd(%ebp)
 
-# wykładnik gotowy
-# Przesunięcie mantysy w lewo żeby pozbyć się najstarszego bitu, który jest częścią wykładnika
-movb 0x12(%ebp), %cl # c
-movb 0x11(%ebp), %dl # b
-movb 0x10(%ebp), %ch # a
+    push $9
+    push %edi
+    call simple_shiftL
 
-shlb $1, %ch
-rclb $1, %dl
-rclb $1, %cl
+    push $1
+    push %edi
+    call simple_shiftR
 
-movb %cl, 0x12(%ebp)
-movb %dl, 0x11(%ebp)
-movb %ch, 0x10(%ebp)
+    movb $3, %cl
+    addb $128, (%edi, %ecx, 1)
 
-jmp SECOND_CASE
+    push $1
+    push %edi
+    call simple_shiftL_64
 
-FIRST_CASE:
+    movb $0, %cl
+    addb $1, (%ebx, %ecx, 1)
 
-movb 0x12(%ebp), %cl # c
-movb 0x11(%ebp), %dl # b
-movb 0x10(%ebp), %al # a
-
-shrb $1, %al
-rcrb $1, %dl
-rcrb $1, %cl
-
-movb $1, %al
-rclb $1, %al
-
-# W al są teraz dwie pierwsze cyfry mantysy, te przed przecinkiem. Dwie bo skalowaliśmy mantyse
-
-# Tu zapisywany będzie aktualny wynik pierwiastkowania mantysy
-movb $0, -0x8(%ebp)
-movb $0, -0x7(%ebp)
-movb $0, -0x6(%ebp)
-movb $1, -0x5(%ebp)
+    push %ebx
+    push %esi
+    call simple_sub
 
 
-subb $1, %al
+    movb $0, %dl
+
+ALGORITHM_LOOP:
+
+    cmpb $24, %dl
+    je ALGORITHM_LOOP_EXIT
+
+    push $2
+    push %edi
+    call simple_shiftL_64
+
+    push $2
+    push %ebx
+    call simple_shiftL
+
+    movb $0, %cl
+    addb $1, (%ebx, %ecx, 1)
 
 
-# Tu zapisywana będzie aktualna reszta
+    movb $3, %cl
+LOOP:    
+    cmpb $0, %cl
+    jl X_1
+    movb (%esi, %ecx, 1), %al
+    movb (%ebx, %ecx, 1), %ah
+    cmpb %ah, %al
+    jb X_0
+    cmpb %ah, %al
+    je DALEJ
+    jmp X_1
+DALEJ:
 
-movb 0x12(%ebp), %cl # c
-movb 0x11(%ebp), %dl # b
-movb 0x10(%ebp), %ch # a
-
-shlb $1, %ch
-rclb $1, %dl
-rclb $1, %cl
-rclb $1, %al
-
-shlb $1, %ch
-rclb $1, %dl
-rclb $1, %cl
-rclb $1, %al
-
-movb %cl, 0x12(%ebp)
-movb %dl, 0x11(%ebp)
-movb %ch, 0x10(%ebp)
-
-
-movb $0, -0xc(%ebp)
-movb $0, -0xb(%ebp)
-movb $0, -0xa(%ebp)
-movb %al, -0x9(%ebp)
-
-
-xor %dh, %dh
-jmp HARD
-
-SECOND_CASE:
-
-# Tu zapisywany będzie aktualny wynik pierwiastkowania mantysy
-movb $0, -0x8(%ebp)
-movb $0, -0x7(%ebp)
-movb $0, -0x6(%ebp)
-movb $1, -0x5(%ebp)
-
-xor %al, %al
-
-movb 0x12(%ebp), %cl # c
-movb 0x11(%ebp), %dl # b
-movb 0x10(%ebp), %ch # a
-
-shlb $1, %ch
-rclb $1, %dl
-rclb $1, %cl
-rclb $1, %al
-
-shlb $1, %ch
-rclb $1, %dl
-rclb $1, %cl
-rclb $1, %al
-
-movb %cl, 0x12(%ebp)
-movb %dl, 0x11(%ebp)
-movb %ch, 0x10(%ebp)
-
-# Tu zapisywana będzie aktualna reszta
-movb $0, -0xc(%ebp)
-movb $0, -0xb(%ebp)
-movb $0, -0xa(%ebp)
-movb %al, -0x9(%ebp)
-
-HARD:
-
-cmpb $22, %dh
-je HARD_EXIT
-
-movb -0x8(%ebp), %al
-movb -0x7(%ebp), %ah
-movb -0x6(%ebp), %cl
-movb -0x5(%ebp), %dl
-
-shlb $1, %dl
-rclb $1, %cl
-rclb $1, %ah
-rclb $1, %al
-
-shlb $1, %dl
-rclb $1, %cl
-rclb $1, %ah
-rclb $1, %al
-
-addb $1, %dl
-adcb $0, %cl
-adcb $0, %ah
-adcb $0, %al
-
-# W rejestrach mam teraz (2 * Qi * 2 + 1)*1. Porównuje to z aktualną resztą
-
-movb -0xc(%ebp), %ch
-cmpb %ch, %al
-jb X_1
-cmpb %al, %ch
-jb X_0
-
-movb -0xb(%ebp), %al
-cmpb %al, %ah
-jb X_1
-cmpb %ah, %al
-jb X_0
-
-movb -0xa(%ebp), %al
-cmpb %al, %cl
-jb X_1
-cmpb %cl, %al
-jb X_0
-
-movb -0x9(%ebp), %al
-cmpb %al, %dl
-jb X_1
-cmpb %dl, %al
-jb X_0
-jmp X_1
-
+    decb %cl
+    jmp LOOP
 X_1:
-movb $20, %al
-addb $255, %al
 
-movb -0x8(%ebp), %al
-movb -0x7(%ebp), %ah
-movb -0x6(%ebp), %cl
-movb -0x5(%ebp), %dl
+    push %ebx
+    push %esi
+    call simple_sub
 
-rclb $1, %dl
-rclb $1, %cl
-rclb $1, %ah
-rclb $1, %al
+    push $2
+    push %ebx
+    call simple_shiftR
 
-movb %al, -0x8(%ebp)
-movb %ah, -0x7(%ebp)
-movb %cl, -0x6(%ebp)
-movb %dl, -0x5(%ebp)
+    push $1
+    push %ebx
+    call simple_shiftL
 
-shrb $1, %al
-rcrb $1, %ah
-rcrb $1, %cl
-rcrb $1, %dl
+    movb $0, %cl
+    addb $1, (%ebx, %ecx, 1)
 
-
-shlb $1, %dl
-rclb $1, %cl
-rclb $1, %ah
-rclb $1, %al
-
-shlb $1, %dl
-rclb $1, %cl
-rclb $1, %ah
-rclb $1, %al
-
-addb $1, %dl
-adcb $0, %cl
-adcb $0, %ah
-adcb $0, %al
-
-movb -0x9(%ebp), %ch
-subb %dl, %ch
-movb %ch, -0x9(%ebp)
-
-movb -0xa(%ebp), %dl
-sbbb %cl, %dl
-movb %dl, -0xa(%ebp)
-
-movb -0xb(%ebp), %dl
-sbbb %ah, %dl
-movb %dl, -0xb(%ebp)
-
-movb -0xc(%ebp), %dl
-sbbb %al, %dl
-movb %dl, -0xc(%ebp)
-
-# reszta odjęta i zapisana wyzej. Teraz dodaje kolejne dwie pozycje mantysy do reszty :(
-# pierwsza pozycja
-movb 0x12(%ebp), %cl # c
-movb 0x11(%ebp), %dl # b
-movb 0x10(%ebp), %ch # a
-
-shlb $1, %ch
-rclb $1, %dl
-rclb $1, %cl
-
-movb %cl, 0x12(%ebp)
-movb %dl, 0x11(%ebp)
-movb %ch, 0x10(%ebp) 
-
-movb -0xc(%ebp), %al
-movb -0xb(%ebp), %ah
-movb -0xa(%ebp), %cl
-movb -0x9(%ebp), %dl
-
-rclb $1, %dl
-rclb $1, %cl
-rclb $1, %ah
-rclb $1, %al
-
-movb %al, -0xc(%ebp)
-movb %ah, -0xb(%ebp)
-movb %cl, -0xa(%ebp)
-movb %dl, -0x9(%ebp)
-
-# druga
-movb 0x12(%ebp), %cl # c
-movb 0x11(%ebp), %dl # b
-movb 0x10(%ebp), %ch # a
-
-shlb $1, %ch
-rclb $1, %dl
-rclb $1, %cl
-
-movb %cl, 0x12(%ebp)
-movb %dl, 0x11(%ebp)
-movb %ch, 0x10(%ebp) 
-
-movb -0xc(%ebp), %al
-movb -0xb(%ebp), %ah
-movb -0xa(%ebp), %cl
-movb -0x9(%ebp), %dl
-
-rclb $1, %dl
-rclb $1, %cl
-rclb $1, %ah
-rclb $1, %al
-
-movb %al, -0xc(%ebp)
-movb %ah, -0xb(%ebp)
-movb %cl, -0xa(%ebp)
-movb %dl, -0x9(%ebp)
-
-
-addb $1, %dh
-jmp HARD
+    incb %dl
+    jmp ALGORITHM_LOOP
 
 X_0:
-# Przesuwam dodając 0. Od reszty nie musze nic odejmować, więc później dodaje do niej po 
-# prostu kolejne dwie pozycje mantysy
-movb -0x8(%ebp), %al
-movb -0x7(%ebp), %ah
-movb -0x6(%ebp), %cl
-movb -0x5(%ebp), %dl
 
-shlb $1, %dl
-rclb $1, %cl
-rclb $1, %ah
-rclb $1, %al
+    push $2
+    push %ebx
+    call simple_shiftR
 
-movb %al, -0x8(%ebp)
-movb %ah, -0x7(%ebp)
-movb %cl, -0x6(%ebp)
-movb %dl, -0x5(%ebp)
+    push $1
+    push %ebx
+    call simple_shiftL
 
-# Teraz dodaje kolejne dwie pozycje mantysy do reszty :(
-# pierwsza pozycja
+    incb %dl
+    jmp ALGORITHM_LOOP
 
-movb 0x12(%ebp), %cl # c
-movb 0x11(%ebp), %dl # b
-movb 0x10(%ebp), %ch # a
+ALGORITHM_LOOP_EXIT:
 
-shlb $1, %ch
-rclb $1, %dl
-rclb $1, %cl
+    movb $3, %cl
+    movb -0xd(%ebp), %al
+    movb %al, (%ebx, %ecx, 1)
 
-movb %cl, 0x12(%ebp)
-movb %dl, 0x11(%ebp)
-movb %ch, 0x10(%ebp) 
 
-movb -0xc(%ebp), %al
-movb -0xb(%ebp), %ah
-movb -0xa(%ebp), %cl
-movb -0x9(%ebp), %dl
+ADDING_THE_SIGN:
 
-rclb $1, %dl
-rclb $1, %cl
-rclb $1, %ah
-rclb $1, %al
+    push $1
+    push %ebx
+    call simple_shiftR
 
-movb %al, -0xc(%ebp)
-movb %ah, -0xb(%ebp)
-movb %cl, -0xa(%ebp)
-movb %dl, -0x9(%ebp)
 
-# druga
-movb 0x12(%ebp), %cl # c
-movb 0x11(%ebp), %dl # b
-movb 0x10(%ebp), %ch # a
+    movl 0x0(%ebx), %ecx
+    movl 0x10(%ebp), %edi
+    movl %ecx, 0x0(%edi)
 
-shlb $1, %ch
-rclb $1, %dl
-rclb $1, %cl
-
-movb %cl, 0x12(%ebp)
-movb %dl, 0x11(%ebp)
-movb %ch, 0x10(%ebp) 
-
-movb -0xc(%ebp), %al
-movb -0xb(%ebp), %ah
-movb -0xa(%ebp), %cl
-movb -0x9(%ebp), %dl
-
-rclb $1, %dl
-rclb $1, %cl
-rclb $1, %ah
-rclb $1, %al
-
-movb %al, -0xc(%ebp)
-movb %ah, -0xb(%ebp)
-movb %cl, -0xa(%ebp)
-movb %dl, -0x9(%ebp)
-
-addb $1, %dh
-jmp HARD
-
-HARD_EXIT:
-
-    movb -0x7(%ebp), %ah
-    movb -0x6(%ebp), %cl
-    movb -0x5(%ebp), %dl
-
-SHIFTING:
-
-    shlb $1, %dl
-    rclb $1, %cl
-    rclb $1, %ah
-
-    jc SHIFTING_EXIT
-    JMP SHIFTING    
-
-SHIFTING_EXIT:
-
-    movb -0x1(%ebp), %al
-
-    shrb $1, %al
-    rcrb $1, %ah
-    rcrb $1, %cl
-    rcrb $1, %dl
-
-    movl	0xc(%ebp), %edi
-	movb	%al, 0x3(%edi)
-	movb	%ah, 0x2(%edi)
-	movb	%cl, 0x1(%edi)
-	movb	%dl, 0x0(%edi)
-
-	movl	%ebp, %esp
+    movl	%ebp, %esp
+    popl    %esi
 	popl	%edi
-	popl	%ebp
-	ret
+    popl    %ebp
+    ret
--- tests/float/single/singleSqrtTest.cpp
+++ tests/float/single/singleSqrtTest.cpp
@@ -7,23 +7,73 @@ TEST_CASE("Square root 1", "")
 {
     GIVEN("New Single objects")
     {
-        Single a = 5.0_s;
+        Single a = 2.0_s;
         WHEN("square root is made")
         {
             a = a.sqrt();
 
             THEN("value is correct more or less")
             {
-                Single expected = 2.23606777f;
+                Single expected = 1.41421356f;
                 REQUIRE((bool)(expected == a));
             }
 
             // TODO: rounding mode
-            /* THEN("value is correct with float")
+            THEN("value is correct with float")
             {
-                float expected = std::sqrt(5);
+                float expected = std::sqrt(2);
                 REQUIRE((bool)(a == expected));
-            } */
+            }
+        }
+    }
+}
+
+TEST_CASE("Square root 2", "")
+{
+    GIVEN("New Single objects")
+    {
+        Single a = 6.0_s;
+        WHEN("square root is made")
+        {
+            a = a.sqrt();
+
+            THEN("value is correct more or less")
+            {
+                Single expected = 2.44948959f;
+                REQUIRE((bool)(expected == a));
+            }
+
+            // TODO: rounding mode
+            THEN("value is correct with float")
+            {
+                float expected = std::sqrt(6);
+                REQUIRE((bool)(a == expected));
+            }
+        }
+    }
+}
+
+TEST_CASE("Square root 3", "")
+{
+    GIVEN("New Single objects")
+    {
+        Single a = 9.0_s;
+        WHEN("square root is made")
+        {
+            a = a.sqrt();
+
+            THEN("value is correct more or less")
+            {
+                Single expected = 3.0f;
+                REQUIRE((bool)(expected == a));
+            }
+
+            // TODO: rounding mode
+            THEN("value is correct with float")
+            {
+                float expected = std::sqrt(9);
+                REQUIRE((bool)(a == expected));
+            }
         }
     }
 }
